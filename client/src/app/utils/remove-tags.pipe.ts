461ace8a248854a9c6e76a0bb7aae71f68b51685
Johnny Richardson <johnny.c.rich@gmail.com>
2018-06-26 11:45:16 -0400

    Resolved HTML entities showing in opp description (#129).

--- client/src/app/utils/remove-tags.pipe.ts
+++ client/src/app/utils/remove-tags.pipe.ts
@@ -6,11 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RemoveTagsPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    var re = new RegExp("<\s*p[^>]*>(.*?)<\s*/\s*p>");
-    var arr = re.exec(value);
 
-    if(arr)
-        return arr[1];
+    const doc =  new DOMParser().parseFromString(value, 'text/html');
+    return doc.documentElement.textContent;
+
   }
 
 }
