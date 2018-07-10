d72c1e30496fa57d93be10d77f550eaa2fd664e4
Johnny Richardson <johnny.c.rich@gmail.com>
2018-07-10 11:31:44 -0400

    Updated character images and improved layout for character on home screen.

--- client/src/app/game/start/start.component.ts
+++ client/src/app/game/start/start.component.ts
@@ -13,6 +13,7 @@ export class GameStartComponent implements OnInit {
         
         TweenLite.fromTo(document.getElementById('logo'), 1, {scale:0}, {scale:1, delay: 2, display:'block', ease:Elastic.easeOut});
         TweenLite.fromTo(document.getElementById('new'), 1, {scale:0}, {scale:1, delay: 2.5, visibility:'visible', ease:Elastic.easeOut});
+        TweenLite.fromTo(document.getElementById('footer'), .5, {scale:0}, {scale:1, delay: 2.7, visibility:'visible', ease:Back.easeOut});
         TweenLite.fromTo(document.getElementById('bg'), 1, {alpha:0}, {alpha:1, delay: 2.7, visibility:'visible'});
 
     }
@@ -21,8 +22,10 @@ export class GameStartComponent implements OnInit {
 
         let start = document.getElementById('start');
         let characters = document.getElementById('characters-parent');
+        let footer = document.getElementById('footer');
 
         TweenLite.fromTo(start, .7, {autoAlpha:1}, {autoAlpha:0, left:'-100%', display:'none', ease:Back.easeIn});
+        TweenLite.fromTo(footer, .7, {autoAlpha:1}, {autoAlpha:0, top:'100%', display:'none', ease:Back.easeIn});
         TweenLite.fromTo(characters, .7, {autoAlpha:1, left:'100%'}, {autoAlpha:1, left:0, delay:.7, display:'block', ease:Back.easeOut});
 
     }
